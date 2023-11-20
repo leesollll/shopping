ddc2837e2235114acd4bdfb8d8a032ca45a59d3a
leesollll <150757237+leesollll@users.noreply.github.com>
2023-11-20 20:16:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -122,3 +122,12 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+
+// 회원가입
+function redirectToSignUp() {
+
+    window.location.href = 'signup.html';
+}
+
